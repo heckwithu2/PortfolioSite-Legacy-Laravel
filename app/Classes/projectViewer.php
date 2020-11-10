4e128f016b1e0525fedf0a5160eddd3d77e4df06
heckwithu2 <jheck10@kent.edu>
2020-11-10 09:42:07 -0500

    sass live compilation support

--- app/Classes/projectViewer.php
+++ app/Classes/projectViewer.php
@@ -71,9 +71,9 @@ class projectViewer {
 
         echo "<div class='p-0 col-5'>";
         echo "<div class=' iconRow d-flex justify-content-end'>";
-        echo "<a id='git' href='https://github.com/heckwithu2?tab=projects'><img class='socialIcon icon img-fluid' src='" . asset('public/images/github.png')  . "'></a>";
-        echo "<a id='li' href='https://www.linkedin.com/in/jeremiah-heck-498b1a184/'><img class='socialIcon icon img-fluid' src='" . asset('public/images/linkedin2.png') . "'> </a>";                           
-        echo "<a id='mail' href='mailto: jheck10@kent.edu'><img class='socialIcon icon img-fluid' src='" . asset('public/images/gmail.png') . "'></a>";
+        echo "<a id='git' href='https://github.com/heckwithu2?tab=projects' target='_blank'><img class='socialIcon icon img-fluid' src='" . asset('public/images/github.png')  . "'></a>";
+        echo "<a id='li' href='https://www.linkedin.com/in/jeremiah-heck-498b1a184/' target='_blank'><img class='socialIcon icon img-fluid' src='" . asset('public/images/linkedin2.png') . "'> </a>";                           
+        echo "<a id='mail' href='mailto: jheck10@kent.edu' ><img class='socialIcon icon img-fluid' src='" . asset('public/images/gmail.png') . "'></a>";
         echo "</div>";
         echo "</div>";
         echo "</div>";
@@ -91,7 +91,7 @@ class projectViewer {
             echo "<div  class='row categoryRow p-0'>";
                 
                     echo "<div class='col-12 categoryTitle'>";
-                    echo "<a class='aTagRemove' id='" . $this->nameArray[$i] . "' onclick='categoryToProject(this.id)' href='#" . $trimmedName . "'>";
+                    echo "<a class='aTagRemove' id='" . $this->nameArray[$i] . "' onclick='categoryToProject(this.id)' href='#" . $trimmedName . "' >";
                     echo "<p  class='categoryText'>" . $this->nameArray[$i] . "</p>";
                         
                         $projects = array();
